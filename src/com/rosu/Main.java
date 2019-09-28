77e3b1658aecf72484e45ea456c9d4eea5de3bf7
rflorian2017 <roby_florian@yahoo.com>
2019-09-28 11:33:07 +0300

    Added read from console of user and password

--- src/com/rosu/Main.java
+++ src/com/rosu/Main.java
@@ -1,25 +1,43 @@
 package com.rosu;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Main {
 
+
     public static void main(String[] args) {
+        String user = "";
+        String password = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.println("Please input user:");
+            user = reader.readLine();
+
+            System.out.println("Please input password:");
+            password = reader.readLine();
+
+        }
+        catch (IOException ex) {
+
+        }
+
         // write your code here
         Connection connection; // used to open a connection to a database
         try {
             connection = DriverManager.
                     getConnection("jdbc:mysql://localhost:3306",
-                            "root",
-                            "admin");
+                            user,//"root",
+                            password);//"admin");
             System.out.println("Connection successfully");
         }
         catch (Exception ex) {
             System.out.println(ex.getMessage());
-            for (StackTraceElement entry:ex.getStackTrace()
-                 ) {
+            for (StackTraceElement entry:ex.getStackTrace()) {
                 System.out.println(entry.getMethodName());
             }
 
