282b5fe131c1371c50ad75edc9389b3c9ba12608
rflorian2017 <roby_florian@yahoo.com>
2019-09-28 11:10:51 +0300

    Iniatial commit to local repo

---
+++ src/com/rosu/Main.java
@@ -0,0 +1,30 @@
+package com.rosu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Connection connection; // used to open a connection to a database
+        try {
+            connection = DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306",
+                            "root",
+                            "admin2");
+            System.out.println("Connection successfully");
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            for (StackTraceElement entry:ex.getStackTrace()
+                 ) {
+                System.out.println(entry.getMethodName());
+            }
+
+        }
+
+
+    }
+}
