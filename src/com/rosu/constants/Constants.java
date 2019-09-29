e91ddf627d284ebfadff54965a99ff3b0f5bc4e6
rflorian2017 <roby_florian@yahoo.com>
2019-09-29 15:26:06 +0300

    Models for hibernate

--- src/com/rosu/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.rosu.constants;
-
-public class Constants {
-    public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "admin";
-
-    public static final String DB_DRIVER_IMPLEMENTATION = "jdbc:mysql";
-    public static final String DB_CONN_STRING = "localhost:3306/imdb";
-}
